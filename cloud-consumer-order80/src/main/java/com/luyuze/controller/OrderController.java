77935f70531e45f439fbd47404f245c595678e71
luyuze <627668587@qq.com>
2020-07-13 16:58:34 +0800

    common add

--- cloud-consumer-order80/src/main/java/com/luyuze/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/luyuze/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.luyuze.controller;
 import com.luyuze.entity.CommonResult;
 import com.luyuze.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
