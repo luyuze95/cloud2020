fc6a8a5bdfb32d2fa5b689c68d4658b417ae73f3
luyuze <627668587@qq.com>
2020-07-21 22:54:25 +0800

    ribbon set

--- cloud-consumer-order80/src/main/java/com/luyuze/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/luyuze/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.luyuze.controller;
 import com.luyuze.entity.CommonResult;
 import com.luyuze.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -34,5 +35,18 @@ public class OrderController {
                 PAYMENT_URL + "/payment/get/" + id,
                 CommonResult.class
         );
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(
+                PAYMENT_URL + "/payment/get/" + id,
+                CommonResult.class
+        );
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
     }
 }
