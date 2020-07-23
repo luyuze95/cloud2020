093c1ee43da12ce063f13ae8860faaeebb96acdf
luyuze <627668587@qq.com>
2020-07-23 22:44:41 +0800

    OpenFeign add

---
+++ cloud-consumer-feign-order80/src/main/java/com/luyuze/controller/OrderFeignController.java
@@ -0,0 +1,23 @@
+package com.luyuze.controller;
+
+import com.luyuze.entity.CommonResult;
+import com.luyuze.entity.Payment;
+import com.luyuze.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
