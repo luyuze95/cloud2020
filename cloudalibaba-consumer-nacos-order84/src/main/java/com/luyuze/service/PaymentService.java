45cfc5378e5f94e4fbc3df58851a8a9e07991293
luyuze <627668587@qq.com>
2020-08-09 13:01:26 +0800

    sentinel finish

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/luyuze/service/PaymentService.java
@@ -0,0 +1,14 @@
+package com.luyuze.service;
+
+import com.luyuze.entity.CommonResult;
+import com.luyuze.entity.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "nacos-payment-provider", fallback = PaymentFallbackService.class)
+public interface PaymentService {
+    @GetMapping(value = "/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id);
+}
+
